fdbb39906fd1b7fead6ca5b8cd896a7a2d999afe
Tianyu Zhang <dowdah@163.com>
2024-07-05 21:25:01 +0800

    Add permissions route for the frontend to get permission details

--- backend/app/api/v1/views.py
+++ backend/app/api/v1/views.py
@@ -1,10 +1,20 @@
 from . import v1_bp
 from flask import current_app, jsonify
-# from ...models import User
+from ...models import Permission
 
 
 @v1_bp.route('/task/<task_id>')
 def get_task(task_id):
     task = current_app.celery.AsyncResult(task_id)
     response_json = {'success': True, 'code': 200, 'task_status': task.status, 'task_result': task.result}
+    return jsonify(response_json), response_json['code']
+
+
+@v1_bp.route('/permissions')
+def get_permission_info():
+    response_json = {
+        'success': True,
+        'code': 200,
+        'permissions': Permission.to_json()
+    }
     return jsonify(response_json), response_json['code']
