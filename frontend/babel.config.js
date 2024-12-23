651f1f5dbd3f36d3f334eb1765bd4b6b231cceb5
Tianyu Zhang <dowdah@163.com>
2024-12-23 21:17:43 +0800

    remove babel

--- frontend/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    ['import', {
-      libraryName: '@ant-design/icons-vue',
-      libraryDirectory: 'es', // 使用 'es' 目录
-      camel2DashComponentName: false // 禁用驼峰命名转换
-    }, '@ant-design/icons-vue']
-  ]
-};
