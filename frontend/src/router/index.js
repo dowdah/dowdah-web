a807da573d496eaf172736663aa789c077209fcf
Tianyu Zhang <dowdah@163.com>
2024-12-25 20:45:49 +0800

    Add custom 404 and 403 pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import store from '../store';
 import Home from '../views/Home.vue';
+import NotFound from "../views/NotFound.vue";
+import PermissionDenied from "../views/PermissionDenied.vue";
 
 const routes = [
     {path: '/', name: '主页', component: Home},
@@ -8,6 +10,8 @@ const routes = [
     {path: '/test2', name: '测试2', component: Home, meta: {groupName: "测试组1"}},
     {path: '/test3', name: '测试3', component: Home, meta: {groupName: "测试组2"}},
     {path: '/test4', name: '测试4', component: Home, meta: {groupName: "测试组3"}}
+    {path: '/permission-denied', name: '别看不该看的！', component: PermissionDenied, meta: {notShownInMenu: true}},
+    {path: '/:pathMatch(.*)*', name: '这是哪？', component: NotFound, meta: {notShownInMenu: true}}
 ];
 
 const router = createRouter({history: createWebHistory(), routes});
@@ -17,18 +21,25 @@ router.beforeEach(async (to, from, next) => {
     console.log(`Navigating to: ${to.name}`);
     store.commit('setTopBarTitle', '主页');
     if (to.matched.some(record => record.meta.requiresAuth) && !store.getters.isAuthenticated) {
-        console.log('Not authenticated, redirecting to Home');
-        next({name: '主页'});
+        console.log('Not authenticated, redirecting to 403 page');
+        next({name: '别看不该看的！'});
     }
     if (to.matched.some(record => record.meta.blockWhenAuthenticated) && store.getters.isAuthenticated) {
         console.log('Already authenticated, redirecting to Home');
         next({name: '主页'});
     }
     if (to.matched.some(record => record.meta.requiresPermission !== undefined)) {
+        if (!store.getters.isAuthenticated) {
+            console.log('Not authenticated, redirecting to 403 page');
+            next({name: '别看不该看的！'});
+            return;
+        }
         for (let permission of to.meta.requiresPermission) {
             if (!store.getters.hasPermission(permission)) {
                 console.log(`Current user can't ${permission}, redirecting to Home`);
                 next({name: '主页'});
+                console.log(`Current user can't ${permission}, redirecting to 403 page`);
+                next({name: '别看不该看的！'});
                 return;
             }
         }
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-result status="404" title="404" sub-title="给我干哪来了？这还是地球吗？">
+    <template #extra>
+      <a-button type="primary" @click="$router.push('/')">回家</a-button>
+    </template>
+  </a-result>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
