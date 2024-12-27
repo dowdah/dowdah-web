24e838b95b4269ce5765971659b798c97123d7d9
Tianyu Zhang <dowdah@163.com>
2024-12-28 01:45:10 +0800

    Fix Vue WebSocket Function(Now fully supports HMR!!!!)

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,9 @@ module.exports = {
   devServer: {
     port: 80,
     allowedHosts: ['dowdah.com', 'localhost', '127.0.0.1', 'www.dowdah.com'],
+    hot: true, // 启用 HMR
+    client: {
+      webSocketURL: 'ws://localhost:80/ws', // 配置 WebSocket URL
+    },
   },
 };
