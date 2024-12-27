034a7768b44eead6a0fcaa813303e6ac42f7ae64
Tianyu Zhang <dowdah@163.com>
2024-12-27 09:37:48 +0800

    Add "My" View

---
+++ frontend/src/views/My.vue
@@ -0,0 +1,15 @@
+<template>
+  <Dashboard />
+</template>
+
+<script>
+import Dashboard from "../components/Dashboard.vue";
+
+export default {
+  name: 'Test',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
