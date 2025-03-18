5e2be3f9f36ea358612799fec68eeeb6ad4c6ba4
Tianyu Zhang <dowdah@163.com>
2025-03-18 23:38:38 +0800

    feat(constants.js): add Turnstile site key and verification URL

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -2,4 +2,5 @@ export const API_VERSION = 'v1';
 export const BASE_API_URL = `/api/${API_VERSION}`;
 export const ROLE_NAMES = ['User', 'Administrator'];
 export const SITE_NAME = 'Dowdah'
-export const AVATAR_PROXY = 'https://avatar-proxy.dowdah.com';
+export const TURNSTILE_SITE_KEY = '0x4AAAAAABBDvaxOXhwMWkni'
+export const TURNSTILE_VERIFY_URL = 'https://turnstile-verify.dowdah.com/'
